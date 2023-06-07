96cdf2a3a5ab772a6f9d2e7af766fb1f858aca43
Nikhil <nikhilshah.net@hotmail.com>
2023-06-07 10:40:00 +0530

    Project initlized to GIT

---
+++ axaskilling_vs/axaskilling_vs/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace axaskilling_vs.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
